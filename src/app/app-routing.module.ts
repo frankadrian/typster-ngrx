b9c760e35ddbdd60034398d8981187dd32411d25
Frank Adrian <frankwebdevelopment@gmail.com>
2020-01-23 15:43:57 +0100

    Need Common Module in Routing module with lazy imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {WelcomeComponent} from './components/welcome/welcome.component';
+import {CommonModule} from '@angular/common';
 
 const routes: Routes = [
   {
     path: '',
     component: WelcomeComponent
   },
-
-  { path: 'typetest', loadChildren: () => import('./modules/typetest/typetest.module').then(m => m.TypetestModule) },
+  {
+    path: 'typetest',
+    loadChildren: () => import('./modules/typetest/typetest.module').then(m => m.TypetestModule)
+  },
   { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
